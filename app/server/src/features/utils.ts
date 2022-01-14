28429f778c00f09775265c6ee63dc8e479866fcf
kadey001 <kadey001@ucr.edu>
2022-01-13 18:34:19 -0800

    feat(errors): added email error to common error list

--- app/server/src/features/utils.ts
+++ app/server/src/features/utils.ts
@@ -34,6 +34,7 @@ export const errors = {
     permissions: 'Insufficient permissions',
     fileNotFound: 'File not found',
     fileSize: 'File too large',
+    email: 'Emailed failed to send due to an unexpected error'
 };
 interface TFilterFieldArgs<TObj extends Record<string, unknown>, TKeys extends keyof TObj> {
     input: TObj;
