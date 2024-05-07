1f8323fc7e1260e547ac451e3b346a93d1f5726f
kadey001 <kadey001@ucr.edu>
2024-05-06 18:16:50 -0700

    fix(graceful shutdown): updated to ensure server is being shutdown properly

--- app/server/src/core/graceful-shutdown.ts
+++ app/server/src/core/graceful-shutdown.ts
@@ -1,19 +1,36 @@
-import type { FastifyLoggerInstance } from 'fastify';
+import type { FastifyBaseLogger } from 'fastify';
+import { getOrCreateServer } from './server';
 import { getRedisClient } from './utils';
 
-// TODO: make this much more robust.
-export function initGracefulShutdown(logger: FastifyLoggerInstance) {
+export function initGracefulShutdown(logger: FastifyBaseLogger) {
     const cleanup = () => {
+        logger.info('Gracefully shutting down...');
+        const server = getOrCreateServer();
         const redis = getRedisClient(logger);
-        try {
-            redis.quit().then(() => {
+
+        logger.info('Closing redis client...');
+        redis
+            .quit()
+            .then(() => {
                 logger.info('Redis client closed.');
+                logger.info('Closing server...');
+                server
+                    .close()
+                    .then(() => {
+                        logger.info('Server closed.');
+                        process.exit(0);
+                    })
+                    .catch((err) => {
+                        logger.error(err);
+                        process.exit(1);
+                    });
+            })
+            .catch((err) => {
+                logger.error(err);
             });
-        } catch (err) {
-            logger.error(err);
-        }
     };
 
-    process.on('exit', cleanup);
+    process.on('SIGINT', cleanup);
+    process.on('SIGTERM', cleanup);
     process.on('uncaughtException', cleanup);
 }
