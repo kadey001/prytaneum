e1f0453df0eca3fa73f21b1ca3a9521ee4880e06
kadey001 <kadey001@ucr.edu>
2024-05-22 19:55:31 -0700

    fix(topic queue hooks): removed updaters in favor of directives

--- app/client/src/features/events/ModeratorView/hooks/TopicQueue/useTopicQueuePush.ts
+++ app/client/src/features/events/ModeratorView/hooks/TopicQueue/useTopicQueuePush.ts
@@ -2,7 +2,7 @@
 import { useTopicQueuePushSubscription } from '@local/__generated__/useTopicQueuePushSubscription.graphql';
 import React from 'react';
 import { useSubscription } from 'react-relay';
-import { ConnectionHandler, graphql, GraphQLSubscriptionConfig } from 'relay-runtime';
+import { graphql, GraphQLSubscriptionConfig } from 'relay-runtime';
 
 const USE_TOPIC_QUEUE_PUSH = graphql`
     subscription useTopicQueuePushSubscription($eventId: String!, $topic: String!, $connections: [ID!]!) {
@@ -37,25 +37,6 @@ export function useTopicQueuePush({ eventId, topic, connections }: Props) {
         () => ({
             subscription: USE_TOPIC_QUEUE_PUSH,
             variables: { eventId, topic, connections },
-            updater: (store) => {
-                // Remove the question from the current question list by topic
-                const eventRecord = store.get(eventId);
-                if (!eventRecord) return console.error('Event Record not found');
-                const connection = ConnectionHandler.getConnectionID(
-                    eventRecord.getDataID(),
-                    'useQuestionsByTopicFragment_questions'
-                );
-                // Need to do this workaround because the compiler in current version doesn't allow correctly naming the connection with _connection at the end.
-                const connectionId = connection + `(topic:"${topic}")`;
-                const connectionRecord = store.get(connectionId);
-                if (!connectionRecord) return console.error('Update failed: Connection record not found!');
-                const payload = store.getRootField('topicQueuePush');
-                if (!payload) return console.error('Update failed: No payload found!');
-                const serverEdge = payload.getLinkedRecord('edge');
-                if (!serverEdge) return console.error('Update failed: No edge found!');
-                const questionId = serverEdge.getLinkedRecord('node').getValue('id');
-                ConnectionHandler.deleteNode(connectionRecord, questionId);
-            },
         }),
         [connections, eventId, topic]
     );
--- app/client/src/features/events/ModeratorView/hooks/TopicQueue/useTopicQueueRemove.ts
+++ app/client/src/features/events/ModeratorView/hooks/TopicQueue/useTopicQueueRemove.ts
@@ -2,7 +2,7 @@
 import { useTopicQueueRemoveSubscription } from '@local/__generated__/useTopicQueueRemoveSubscription.graphql';
 import React from 'react';
 import { useSubscription } from 'react-relay';
-import { ConnectionHandler, graphql, GraphQLSubscriptionConfig } from 'relay-runtime';
+import { graphql, GraphQLSubscriptionConfig } from 'relay-runtime';
 
 const USE_TOPIC_QUEUE_REMOVE = graphql`
     subscription useTopicQueueRemoveSubscription($eventId: String!, $topic: String!, $connections: [ID!]!) {
@@ -10,15 +10,6 @@ const USE_TOPIC_QUEUE_REMOVE = graphql`
             edge {
                 node {
                     id @deleteEdge(connections: $connections)
-                    ...QuestionAuthorFragment
-                    ...QuestionStatsFragment
-                    ...QuestionContentFragment
-                    topics {
-                        topic
-                        position
-                    }
-                    position
-                    onDeckPosition
                 }
                 cursor
             }
@@ -37,33 +28,6 @@ export function useTopicQueueRemove({ eventId, topic, connections }: Props) {
         () => ({
             subscription: USE_TOPIC_QUEUE_REMOVE,
             variables: { eventId, topic, connections },
-            // This updater should fix the unexpected behavior where sometimes the question list subscription isn't updating it.
-            // This is a workaround until we can figure out why the subscription isn't updating the list consistantly.
-            updater: (store) => {
-                const eventRecord = store.get(eventId);
-                if (!eventRecord) return console.error('Update failed: Event record not found!');
-                // Ensure that the question is added back to the question list
-                const questionsByTopicConnection = ConnectionHandler.getConnectionID(
-                    eventRecord.getDataID(),
-                    'useQuestionsByTopicFragment_questions'
-                );
-                const questionsByTopicConnectionId = questionsByTopicConnection + `(topic:"${topic}")`;
-                const questionsByTopicConnectionRecord = store.get(questionsByTopicConnectionId);
-                if (!questionsByTopicConnectionRecord)
-                    return console.error('Update failed: Connection record not found!');
-                const payload = store.getRootField('topicQueueRemove');
-                if (!payload) return console.error('Update failed: No payload found!');
-                const serverEdge = payload.getLinkedRecord('edge');
-                // Check if the question has already been added back to the question list
-                // Need to do this or it can create duplicates when the question list subscription did receive the update
-                // const questionId = serverEdge.getLinkedRecord('node').getValue('id');
-                // const questionAlreadyExists = store.get(questionId);
-                // if (questionAlreadyExists) {
-                //     return console.error('Update failed: Question already exists!');
-                // }
-                if (!serverEdge) return console.error('Update failed: No edge found!');
-                ConnectionHandler.insertEdgeAfter(questionsByTopicConnectionRecord, serverEdge);
-            },
         }),
         [eventId, topic, connections]
     );
