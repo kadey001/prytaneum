aab8d1c18058e9fbb9bd2320a846523717c35901
kadey001 <kadey001@ucr.edu>
2024-05-29 01:55:11 -0700

    fix(topic queue): refactored + error handling + cleanup

--- app/client/src/features/events/ModeratorView/hooks/TopicQueue/useUpdateTopicQueuePosition.ts
+++ app/client/src/features/events/ModeratorView/hooks/TopicQueue/useUpdateTopicQueuePosition.ts
@@ -2,8 +2,10 @@ import React from 'react';
 import { useMutation } from 'react-relay';
 import { graphql } from 'relay-runtime';
 
+import { useSnack } from '@local/core';
 import type { useUpdateTopicQueuePositionMutation } from '@local/__generated__/useUpdateTopicQueuePositionMutation.graphql';
 import { Question } from '../../types';
+import { calculateUpdatedTopicQueuePosition } from './utils';
 
 export const USE_UPDATE_ON_DECK_POSITION = graphql`
     mutation useUpdateTopicQueuePositionMutation($input: UpdateTopicQueuePosition!) {
@@ -27,85 +29,14 @@ export const USE_UPDATE_ON_DECK_POSITION = graphql`
     }
 `;
 
-// TODO: Implement subscription
-// export const USE_UPDATE_TOPIC_QUEUE_POSITION_SUBSCRIPTION = graphql`
-//     subscription useUpdateTopicQueuePositionSubscription($eventId: ID!) {
-//         topicQueuePositionUpdated(eventId: $eventId) {
-//             cursor
-//             node {
-//                 id
-//                 position
-//                 topics {
-//                     position
-//                 }
-//             }
-//         }
-//     }
-// `;
-
 interface Props {
     eventId: string;
     topic: string;
 }
 
 export function useUpdateTopicQueuePosition({ eventId, topic }: Props) {
+    const { displaySnack } = useSnack();
     const [commit] = useMutation<useUpdateTopicQueuePositionMutation>(USE_UPDATE_ON_DECK_POSITION);
-
-    // Gets the position of the question relative to what toic queue it is in
-    // Otherwise, just use the question's position as it should be in the default queue
-    const getTopicListPosition = (question: Question, currentTopic: string) => {
-        const topicPosition = question?.topics?.find((t) => t.topic === currentTopic)?.position;
-        if (!topicPosition) return parseInt(question.position);
-        return parseInt(topicPosition);
-    };
-
-    // NOTE: At this point the list is already updated on client
-    // So the destIndex is the list should give us the moved question (but pos is not updated yet)
-    // Can use this index to find relative question's positions
-    const calculateTopicQueuePosition = React.useCallback(
-        (
-            list: Question[],
-            destIndex: number, // Destination index is where the question is going to
-            currentQuestionPosition: number, // The current question's position (new pos never smaller than this)
-            currentTopic: string
-        ) => {
-            // Since we are re-ordering, we can assume there are at least 2 questions to work with.
-            // First handle if being moved to the top of the list
-            if (destIndex === 0) {
-                // Should be a question after this, but need to check if a current question exists first
-                const nextQuestion = list[1];
-                const nextQuestionTopicPosition = getTopicListPosition(nextQuestion, currentTopic);
-                if (currentQuestionPosition === -1) {
-                    return nextQuestionTopicPosition - 1000;
-                } else {
-                    // Can use both positions to calculate new position in between
-                    const diff = Math.abs(nextQuestionTopicPosition - currentQuestionPosition);
-                    const halfDiff = Math.round(diff / 2);
-                    return currentQuestionPosition + halfDiff;
-                }
-            }
-
-            // Handle if being moved to the bottom of the list
-            if (destIndex === list.length - 1) {
-                // Should be a question before this, can use that to calculate new position
-                const prevQuestion = list[list.length - 2];
-                const prevQuestionTopicPosition = getTopicListPosition(prevQuestion, currentTopic);
-                return prevQuestionTopicPosition + 1000;
-            }
-
-            // Handle if being moved to the middle of the list
-            const nextQuestion = list[destIndex + 1];
-            const prevQuestion = list[destIndex - 1];
-
-            const nextQuestionTopicPosition = getTopicListPosition(nextQuestion, currentTopic);
-            const prevQuestionTopicPosition = getTopicListPosition(prevQuestion, currentTopic);
-
-            const diff = Math.abs(nextQuestionTopicPosition - prevQuestionTopicPosition);
-            const halfDiff = Math.round(diff / 2);
-            return prevQuestionTopicPosition + halfDiff;
-        },
-        []
-    );
 
     type UpdateTopicQueuePositionInput = {
         questionId: string;
@@ -121,12 +52,12 @@ export function useUpdateTopicQueuePosition({ eventId, topic }: Props) {
             const { list, sourceIdx, destinationIdx, currentQuestionPosition, currentTopic } = input;
             // No need to update if the source and destination are the same
             if (sourceIdx === destinationIdx) return;
-            const newPosition = calculateTopicQueuePosition(
+            const newPosition = calculateUpdatedTopicQueuePosition({
                 list,
                 destinationIdx,
                 currentQuestionPosition,
-                currentTopic
-            );
+                currentTopic,
+            });
 
             commit({
                 variables: {
@@ -137,9 +68,17 @@ export function useUpdateTopicQueuePosition({ eventId, topic }: Props) {
                         newPosition: newPosition.toString(),
                     },
                 },
+                onCompleted: (response) => {
+                    if (response.updateTopicQueuePosition.isError) {
+                        return displaySnack(response.updateTopicQueuePosition.message, { variant: 'error' });
+                    }
+                },
+                onError: (error) => {
+                    displaySnack(error.message, { variant: 'error' });
+                },
             });
         },
-        [calculateTopicQueuePosition, commit, eventId, topic]
+        [commit, displaySnack, eventId, topic]
     );
 
     return {
