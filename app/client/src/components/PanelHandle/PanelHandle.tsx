deacc260a7d85ec2fdee05373750df6f7a89022a
kadey001 <kadey001@ucr.edu>
2024-05-29 02:22:20 -0700

    feat(panel handle): added ability to pass alternate icon

--- app/client/src/components/PanelHandle/PanelHandle.tsx
+++ app/client/src/components/PanelHandle/PanelHandle.tsx
@@ -3,18 +3,26 @@ import { Grid, Divider } from '@mui/material';
 import DragHandleIcon from '@mui/icons-material/DragHandle';
 import { PanelResizeHandle } from 'react-resizable-panels';
 
-export function VerticalPanelResizeHandle() {
+interface VerticalPanelResizeHandleProps {
+    alternateIcon?: React.ReactNode;
+}
+
+export function VerticalPanelResizeHandle({ alternateIcon }: VerticalPanelResizeHandleProps) {
     return (
         <PanelResizeHandle>
             <Grid container justifyContent='center' alignItems='center' height='100%'>
                 <Grid container justifyContent='center' height='75%' width='1rem'>
                     <Divider orientation='vertical' variant='middle'>
-                        <DragHandleIcon
-                            sx={{
-                                transform: 'rotate(90deg)',
-                                color: (theme) => theme.palette.grey[300],
-                            }}
-                        />
+                        {alternateIcon ? (
+                            alternateIcon
+                        ) : (
+                            <DragHandleIcon
+                                sx={{
+                                    transform: 'rotate(90deg)',
+                                    color: (theme) => theme.palette.grey[300],
+                                }}
+                            />
+                        )}
                     </Divider>
                 </Grid>
             </Grid>
@@ -22,16 +30,24 @@ export function VerticalPanelResizeHandle() {
     );
 }
 
-export function HorizontalResizeHandle() {
+interface HorizontalPanelResizeHandleProps {
+    alternateIcon?: React.ReactNode;
+}
+
+export function HorizontalResizeHandle({ alternateIcon }: HorizontalPanelResizeHandleProps) {
     return (
         <PanelResizeHandle>
             <Divider sx={{ width: '100%', height: '0.5rem', transform: 'translateY(-0.5rem)' }}>
-                <DragHandleIcon
-                    sx={{
-                        transform: 'translateY(-0.5rem)',
-                        color: (theme) => theme.palette.grey[300],
-                    }}
-                />
+                {alternateIcon ? (
+                    { alternateIcon }
+                ) : (
+                    <DragHandleIcon
+                        sx={{
+                            transform: 'translateY(-0.5rem)',
+                            color: (theme) => theme.palette.grey[300],
+                        }}
+                    />
+                )}
             </Divider>
         </PanelResizeHandle>
     );
