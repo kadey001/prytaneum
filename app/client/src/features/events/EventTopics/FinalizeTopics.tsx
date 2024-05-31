ac1c19ef36289b0844884507fc4cd2a0df253aa3
kadey001 <kadey001@ucr.edu>
2024-05-31 16:23:35 -0700

    fix(finalized topics): memoized list and chips

--- app/client/src/features/events/EventTopics/FinalizeTopics.tsx
+++ app/client/src/features/events/EventTopics/FinalizeTopics.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, Grid, Stack, Chip } from '@mui/material';
+import { Typography, Grid, Stack, Chip, Tooltip } from '@mui/material';
 
 import { getHashedColor } from '@local/core/getHashedColor';
 import { Topic } from './types';
@@ -9,19 +9,26 @@ interface Props {
 }
 
 export function FinalizeTopics({ topics }: Props) {
+    const finalizedTopics = React.useMemo(() => topics, [topics]);
+
+    const memoizedTopicChips = React.useMemo(() => {
+        return finalizedTopics.map((topic, index) => (
+            <Tooltip key={index} title={topic.description}>
+                <Chip
+                    label={topic.topic}
+                    sx={{ color: 'white', backgroundColor: getHashedColor(topic.topic), margin: '0.25rem' }}
+                />
+            </Tooltip>
+        ));
+    }, [finalizedTopics]);
+
     return (
         <Stack direction='column' spacing={2} alignItems='center'>
             <Grid item xs={12}>
-                <Typography variant='body1'>Are you sure you want to finalize the topics?</Typography>
+                <Typography variant='body1'>Are you sure you want to finalize these topics?</Typography>
             </Grid>
             <Grid container justifyContent='center'>
-                {topics.map((topic, index) => (
-                    <Chip
-                        key={index}
-                        label={topic.topic}
-                        sx={{ color: 'white', backgroundColor: getHashedColor(topic.topic), margin: '0.25rem' }}
-                    />
-                ))}
+                {memoizedTopicChips}
             </Grid>
         </Stack>
     );
