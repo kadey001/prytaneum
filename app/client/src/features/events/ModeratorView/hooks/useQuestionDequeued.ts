21af685b5b3572b4a81de74480367bb9f0eaf008
kadey001 <kadey001@ucr.edu>
2024-06-26 17:08:04 -0700

    fix(question list container): removed topic and connections from subscription hooks

--- app/client/src/features/events/ModeratorView/hooks/useQuestionDequeued.ts
+++ app/client/src/features/events/ModeratorView/hooks/useQuestionDequeued.ts
@@ -2,28 +2,33 @@ import { useUser } from '@local/features/accounts';
 import { useQuestionDequeuedSubscription } from '@local/__generated__/useQuestionDequeuedSubscription.graphql';
 import React from 'react';
 import { useSubscription } from 'react-relay';
-import { graphql, GraphQLSubscriptionConfig } from 'relay-runtime';
+import { ConnectionHandler, graphql, GraphQLSubscriptionConfig } from 'relay-runtime';
+import { useEvent } from '../../useEvent';
 
 const USE_QUESTION_DEQUEUED = graphql`
-    subscription useQuestionDequeuedSubscription(
-        $eventId: String!
-        $topic: String!
-        $connections: [ID!]!
-        $lang: String!
-    ) {
-        questionDequeued(eventId: $eventId, topic: $topic) {
-            edge @appendEdge(connections: $connections) {
+    subscription useQuestionDequeuedSubscription($eventId: String!, $lang: String!) {
+        questionDequeued(eventId: $eventId) {
+            edge {
                 node {
                     id
-                    ...QuestionAuthorFragment
-                    ...QuestionStatsFragment
-                    ...QuestionContentFragment @arguments(lang: $lang)
-                    topics {
-                        topic
-                        position
-                    }
+                    question
                     position
                     onDeckPosition
+                    topics {
+                        topic
+                        description
+                        position
+                    }
+                    createdBy {
+                        firstName
+                    }
+                    refQuestion {
+                        ...QuestionQuoteFragment @arguments(lang: $lang)
+                    }
+                    ...QuestionActionsFragment @arguments(lang: $lang)
+                    ...QuestionAuthorFragment
+                    ...QuestionContentFragment @arguments(lang: $lang)
+                    ...QuestionStatsFragment
                 }
                 cursor
             }
@@ -31,21 +36,47 @@ const USE_QUESTION_DEQUEUED = graphql`
     }
 `;
 
-interface Props {
-    eventId: string;
-    topic: string;
-    connections: string[];
-}
-
-export function useQuestionDequeued({ eventId, topic, connections }: Props) {
+export function useQuestionDequeued() {
     const { user } = useUser();
+    const { eventId } = useEvent();
 
     const config = React.useMemo<GraphQLSubscriptionConfig<useQuestionDequeuedSubscription>>(
         () => ({
             subscription: USE_QUESTION_DEQUEUED,
-            variables: { eventId, topic, connections, lang: user?.preferredLang ?? 'EN' },
+            variables: { eventId, lang: user?.preferredLang ?? 'EN' },
+            updater: (store) => {
+                const eventRecord = store.get(eventId);
+                if (!eventRecord) return console.error('Update failed: Event record not found!');
+                // Update all the proper topic lists with the new question (default, and the question's topics)
+
+                const payload = store.getRootField('questionDequeued');
+                if (!payload) return console.error('Update failed: No payload found!');
+                const serverEdge = payload.getLinkedRecord('edge');
+                const topics = serverEdge.getLinkedRecord('node').getLinkedRecords('topics');
+                const topicNames = topics.map((_topic) => _topic.getValue('topic'));
+
+                // Always update the default topic list
+                const questionsByTopicConnection = ConnectionHandler.getConnectionID(
+                    eventRecord.getDataID(),
+                    'useQuestionsByTopicFragment_questionsByTopic'
+                );
+                const connectionId = questionsByTopicConnection + '(topic:"default")';
+                const connectionRecord = store.get(connectionId);
+                if (!connectionRecord)
+                    return console.error(`Update failed: Connection record ${connectionId} not found!`);
+                ConnectionHandler.insertEdgeAfter(connectionRecord, serverEdge);
+
+                // Update the topic lists that are in the question's topics
+                topicNames.forEach((_topic) => {
+                    const _connectionId = questionsByTopicConnection + `(topic:"${_topic}")`;
+                    const _connectionRecord = store.get(_connectionId);
+                    if (!_connectionRecord)
+                        return console.error(`Update failed: Connection record ${_connectionId} not found!`);
+                    ConnectionHandler.insertEdgeAfter(_connectionRecord, serverEdge);
+                });
+            },
         }),
-        [connections, eventId, topic, user?.preferredLang]
+        [eventId, user?.preferredLang]
     );
 
     useSubscription<useQuestionDequeuedSubscription>(config);
--- app/client/src/features/events/ModeratorView/hooks/useQuestionEnqueued.ts
+++ app/client/src/features/events/ModeratorView/hooks/useQuestionEnqueued.ts
@@ -2,33 +2,64 @@
 import { useQuestionEnqueuedSubscription } from '@local/__generated__/useQuestionEnqueuedSubscription.graphql';
 import React from 'react';
 import { useSubscription } from 'react-relay';
-import { graphql, GraphQLSubscriptionConfig } from 'relay-runtime';
+import { ConnectionHandler, graphql, GraphQLSubscriptionConfig } from 'relay-runtime';
+import { useEvent } from '../../useEvent';
+import { useTopic } from '../useTopic';
 
 const USE_QUESTION_ENQUEUED = graphql`
-    subscription useQuestionEnqueuedSubscription($eventId: String!, $topic: String!, $connections: [ID!]!) {
-        questionEnqueued(eventId: $eventId, topic: $topic) {
+    subscription useQuestionEnqueuedSubscription($eventId: String!) {
+        questionEnqueued(eventId: $eventId) {
             edge {
                 node {
-                    id @deleteEdge(connections: $connections)
+                    id
                 }
             }
         }
     }
 `;
 
-interface Props {
-    eventId: string;
-    topic: string;
-    connections: string[];
-}
+export function useQuestionEnqueued() {
+    const { eventId } = useEvent();
+    const { topics } = useTopic();
 
-export function useQuestionEnqueued({ eventId, topic, connections }: Props) {
     const config = React.useMemo<GraphQLSubscriptionConfig<useQuestionEnqueuedSubscription>>(
         () => ({
             subscription: USE_QUESTION_ENQUEUED,
-            variables: { eventId, topic, connections },
+            variables: { eventId },
+            updater: (store) => {
+                const eventRecord = store.get(eventId);
+                if (!eventRecord) return console.error('Update failed: Event record not found!');
+
+                const payload = store.getRootField('questionEnqueued');
+                if (!payload) return console.error('Update failed: No payload found!');
+                const serverEdge = payload.getLinkedRecord('edge');
+                if (!serverEdge) return console.error('Update failed: No edge found!');
+                const node = serverEdge.getLinkedRecord('node');
+                if (!node) return console.error('Update failed: No node found!');
+                const nodeId = node.getValue('id') as string;
+
+                // Always delete from default list
+                const questionsByTopicConnection = ConnectionHandler.getConnectionID(
+                    eventRecord.getDataID(),
+                    'useQuestionsByTopicFragment_questionsByTopic'
+                );
+                const connectionId = questionsByTopicConnection + '(topic:"default")';
+                const connectionRecord = store.get(connectionId);
+                if (!connectionRecord)
+                    return console.error(`Update failed: Connection record ${connectionId} not found!`);
+                ConnectionHandler.deleteNode(connectionRecord, nodeId);
+
+                // Delete from all topic lists
+                topics.forEach(({ topic }) => {
+                    const _connectionId = questionsByTopicConnection + `(topic:"${topic}")`;
+                    const _connectionRecord = store.get(_connectionId);
+                    if (!_connectionRecord)
+                        return console.error(`Update failed: Connection record ${_connectionId} not found!`);
+                    ConnectionHandler.deleteNode(_connectionRecord, nodeId);
+                });
+            },
         }),
-        [connections, eventId, topic]
+        [eventId, topics]
     );
 
     useSubscription<useQuestionEnqueuedSubscription>(config);
