4131cd55e189fc820f05b4181cba73d55eae5918
kadey001 <kadey001@ucr.edu>
2024-10-30 03:18:10 -0700

    chore(prisma): updated so logger is optional param

--- app/server/src/core/utils/prisma.ts
+++ app/server/src/core/utils/prisma.ts
@@ -1,11 +1,14 @@
 import type { FastifyBaseLogger } from 'fastify';
 
 import { PrismaClient } from '@local/__generated__/prisma';
+import { getOrCreateServer } from '../server';
 
 // Prisma client must be a singleton
 let _prisma: PrismaClient | null = null;
+const server = getOrCreateServer();
 
-export function getPrismaClient(logger: FastifyBaseLogger) {
+export function getPrismaClient(_logger?: FastifyBaseLogger) {
+    const logger = _logger || server.log;
     const prisma = _prisma ?? new PrismaClient();
     if (!_prisma) {
         logger.debug('Instantiating new prisma client.');
