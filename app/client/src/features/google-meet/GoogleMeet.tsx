bc88220c5bdd1c6e710423d7e61376bb05acf80b
kadey001 <kadey001@ucr.edu>
2024-11-11 23:39:45 -0800

    chore(google meet): improved error handling + refactor

--- app/client/src/features/google-meet/GoogleMeet.tsx
+++ app/client/src/features/google-meet/GoogleMeet.tsx
@@ -6,13 +6,43 @@ import { useGoogleMeet } from './useGoogleMeet';
 import { useGoogleMeetFragment$key } from '@local/__generated__/useGoogleMeetFragment.graphql';
 import { Loader } from '@local/components';
 
+const LoadingIndicator = () => (
+    <Grid container direction='column' justifyContent='center' alignItems='center' style={{ height: '100%' }}>
+        <Loader />
+    </Grid>
+);
+
+const ReloadButton = ({ onReload }: { onReload: () => void }) => (
+    <Grid container direction='column' justifyContent='center' alignItems='center' style={{ height: '100%' }}>
+        <Typography variant='h6'>The call has ended.</Typography>
+        <Button variant='contained' onClick={onReload}>
+            Reload
+        </Button>
+        <Typography variant='body2' style={{ marginTop: '1rem', padding: '0 .5rem' }}>
+            If you would like to re-join the call click on RELOAD.
+        </Typography>
+        <Typography variant='body2' style={{ marginTop: '1rem', padding: '0 .5rem' }}>
+            If you are having trouble reloading, please refresh the page.
+        </Typography>
+    </Grid>
+);
+
+const ErrorMessage = () => (
+    <Grid container direction='column' justifyContent='center' alignItems='center' style={{ height: '100%' }}>
+        <Typography variant='subtitle1'>Error connecting to Google Meet</Typography>
+        <Button variant='contained' onClick={() => window.location.reload()}>
+            Reload
+        </Button>
+    </Grid>
+);
+
 interface GoogleMeetProps {
     fragmentRef: useGoogleMeetFragment$key;
 }
 
 function GoogleMeet({ fragmentRef }: GoogleMeetProps) {
     const router = useRouter();
-    const { connectToMeeting, displayReloadButton, isLoading } = useGoogleMeet({ fragmentRef });
+    const { connectToMeeting, displayReloadButton, isLoading, isError } = useGoogleMeet({ fragmentRef });
 
     React.useEffect(() => {
         connectToMeeting();
@@ -25,45 +55,11 @@ function GoogleMeet({ fragmentRef }: GoogleMeetProps) {
             <div
                 id='meet-frame-docked'
                 className='meet-frame-docked'
-                style={{ height: displayReloadButton ? '100%' : 'auto' }}
+                style={{ height: displayReloadButton || isError ? '100%' : 'auto' }}
             >
-                {isLoading ? (
-                    <Grid
-                        container
-                        direction='column'
-                        justifyContent='center'
-                        alignItems='center'
-                        style={{ height: '100%' }}
-                    >
-                        <Loader />
-                    </Grid>
-                ) : null}
-                {displayReloadButton ? (
-                    <Grid
-                        container
-                        direction='column'
-                        justifyContent='center'
-                        alignItems='center'
-                        style={{ height: '100%' }}
-                    >
-                        <Typography variant='h6'>The call has ended.</Typography>
-                        <Button variant='contained' onClick={() => router.reload()}>
-                            Reload
-                        </Button>
-                        <Typography
-                            variant='body2'
-                            style={{ marginTop: '1rem', paddingLeft: '.5rem', paddingRight: '.5rem' }}
-                        >
-                            If you would like to re-join the call click on RELOAD.
-                        </Typography>
-                        <Typography
-                            variant='body2'
-                            style={{ marginTop: '1rem', paddingLeft: '.5rem', paddingRight: '.5rem' }}
-                        >
-                            If you are having trouble reloading, please refresh the page.
-                        </Typography>
-                    </Grid>
-                ) : null}
+                {isLoading ? <LoadingIndicator /> : null}
+                {displayReloadButton ? <ReloadButton onReload={() => router.reload()} /> : null}
+                {isError ? <ErrorMessage /> : null}
             </div>
             <div id='meet-frame-pip' className='meet-frame-pip' />
         </React.Fragment>
