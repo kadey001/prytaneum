2a9c7daec45a1244c3e015ff8e77c1116edda911
kadey001 <kadey001@ucr.edu>
2024-11-13 14:54:47 -0800

    chore(utils): added default logger for redis and prisma clients + hash string

--- app/server/src/core/utils/prisma.ts
+++ app/server/src/core/utils/prisma.ts
@@ -1,11 +1,14 @@
 import type { FastifyBaseLogger } from 'fastify';
 
 import { PrismaClient } from '@local/__generated__/prisma';
+import { getOrCreateServer } from '../server';
+
+const server = getOrCreateServer();
 
 // Prisma client must be a singleton
 let _prisma: PrismaClient | null = null;
 
-export function getPrismaClient(logger: FastifyBaseLogger) {
+export function getPrismaClient(logger: FastifyBaseLogger = server.log): PrismaClient {
     const prisma = _prisma ?? new PrismaClient();
     if (!_prisma) {
         logger.debug('Instantiating new prisma client.');
